fba9aa5561f394f443fa1eab926497ac65b37277
Andrew Lau <andrewlau4@hotmail.com>
2023-07-20 13:24:11 -0400

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MaincontentComponent } from './maincontent/maincontent.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
 
-const routes: Routes = [];
+import { authGuard } from './routeguards/auth.guard'
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'maincontent' },
+  {
+    path: 'maincontent',
+    component: MaincontentComponent,
+    canActivate: [ authGuard ],
+  },
+  {
+    path: 'login',
+    component: UserloginComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/routeguards/auth.guard.ts
+++ src/app/routeguards/auth.guard.ts
@@ -1,5 +1,12 @@
 import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const router = inject(Router);
+
+  console.log(`authGuard navigate to login`);
+  return router.navigate(['login']);
+
+  //return true;
 };
