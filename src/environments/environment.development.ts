cd01b563cb4faad6fb26e89704c31f9de2b9ac73
Andrew Lau <andrewlau4@hotmail.com>
2023-07-29 17:10:57 -0400

    added aws config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,25 @@
-export const environment = {};
+export const environment = {
+    awsConfig: {
+        Auth: {
+            identityPoolId: 'x',
+
+            region: 'x',
+        
+            identityPoolRegion: 'x',
+        
+            userPoolId: "x",
+        
+            userPoolWebClientId: "x",
+        
+            mandatorySignIn: false,
+        },
+
+        Storage: {
+            AWSS3: {
+              bucket: 'x',
+              region: 'x', 
+            }
+          },
+    }
+
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,26 @@
-export const environment = {};
+export const environment = {
+    awsConfig: {
+        Auth: {
+            identityPoolId: 'x',
+
+            region: 'x',
+        
+            identityPoolRegion: 'x',
+        
+            userPoolId: "x",
+        
+            userPoolWebClientId: "x",
+        
+            mandatorySignIn: false,
+        },
+
+        Storage: {
+            AWSS3: {
+              bucket: 'x',
+              region: 'x', 
+            }
+          },
+    }
+
+
+};
