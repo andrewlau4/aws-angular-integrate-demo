1cc99ed8fe50771e38acf7374aab4fb616d4f483
Andrew Lau <andrewlau4@hotmail.com>
2023-07-30 18:54:03 -0400

    added file upload logic

--- src/app/predictions/directive/dragndrop-upload.directive.ts
+++ src/app/predictions/directive/dragndrop-upload.directive.ts
@@ -6,10 +6,18 @@ import { Directive, EventEmitter, HostBinding, HostListener, Output } from '@ang
 })
 export class DragndropUploadDirective {
 
-  @HostBinding('class.fileover')
-  fileOver: boolean = false;
+  private cssClasses: string[] = [];
+
+  //@HostBinding('class.fileover')
+  //fileOver: boolean = false;
+
+  @HostBinding('class')
+  classAttributes: string = this.cssClasses.join(" ");
 
   @Output() fileDropped = new EventEmitter<any>();
+
+  readonly fileOverClass = "fileover";
+  readonly redClass = "turn-red";
 
   constructor() { }
 
@@ -19,7 +27,9 @@ export class DragndropUploadDirective {
     evt.preventDefault();
     evt.stopPropagation();
 
-    this.fileOver = true;
+    //this.fileOver = true;
+    this.addClassAttribute(this.fileOverClass, this.redClass);
+    
 
     console.log(`Drag Over ${JSON.stringify(evt)}`);
   }
@@ -30,6 +40,8 @@ export class DragndropUploadDirective {
     evt.stopPropagation();
 
     console.log(`Drag Leave ${JSON.stringify(evt)}`);
+
+    this.clearClassAttribute();
   }
 
   @HostListener('drop', ['$event'])
@@ -37,7 +49,9 @@ export class DragndropUploadDirective {
     evt.preventDefault();
     evt.stopPropagation();
 
-    this.fileOver = false;
+    //this.fileOver = false;
+    this.clearClassAttribute();
+
     const files = evt.dataTransfer.files;
     if (files && files.length > 0) {
       console.log(`Files dropped ${files.length}`);
@@ -48,6 +62,19 @@ export class DragndropUploadDirective {
     }
 
     console.log(`Drag Leave ${JSON.stringify(evt)}`);
+  }
+
+  addClassAttribute(...classNames: string[]) {
+    if (this.cssClasses.length == 0) {
+      this.cssClasses.push(...classNames);
+      console.log(`cssClasses ${this.cssClasses}`)
+      this.classAttributes = this.cssClasses.join(" ");
+    }
+  }
+
+  clearClassAttribute() {
+    this.cssClasses = [];
+    this.classAttributes = "";
   }
 
 }
