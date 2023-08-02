b038b5cc854ba7b2db748a491ba44ed14b4a10bf
Andrew Lau <andrewlau4@hotmail.com>
2023-08-01 22:50:22 -0400

    added configurations

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -20,6 +20,68 @@ export const environment = {
               region: 'x', 
             }
           },
+
+        predictions: {
+          convert: {
+            translateText: {
+              region: 'x',
+              proxy: false,
+              defaults: {
+                sourceLanguage: 'en',
+                targetLanguage: 'zh'
+              }
+            },
+            speechGenerator: {
+              region: 'x',
+              proxy: false,
+              defaults: {
+                VoiceId: 'Ivy',
+                LanguageCode: 'en-US'
+              }
+            },
+            transcription: {
+              region: 'x',
+              proxy: false,
+              defaults: {
+                language: 'en-US'
+              }
+            }
+          },
+          identify: {
+            identifyText: {
+              proxy: false,
+              region: 'x',
+              defaults: {
+                format: 'PLAIN'
+              }
+            },
+            identifyEntities: {
+              proxy: false,
+              region: 'x',
+              celebrityDetectionEnabled: true,
+              defaults: {
+                collectionId: 'identifyEntities8b89c648-test',
+                maxEntities: 50
+              }
+            },
+            identifyLabels: {
+              proxy: false,
+              region: 'x',
+              defaults: {
+                type: 'LABELS'
+              }
+            }
+          },
+          interpret: {
+            interpretText: {
+              region: 'x',
+              proxy: false,
+              defaults: {
+                type: 'ALL'
+              }
+            }
+          }
+        }
     }
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,26 +1,87 @@
 export const environment = {
-    awsConfig: {
-        Auth: {
-            identityPoolId: 'x',
+  awsConfig: {
+      Auth: {
+          identityPoolId: 'x',
 
-            region: 'x',
-        
-            identityPoolRegion: 'x',
-        
-            userPoolId: "x",
-        
-            userPoolWebClientId: "x",
-        
-            mandatorySignIn: false,
+          region: 'x',
+      
+          identityPoolRegion: 'x',
+      
+          userPoolId: "x",
+      
+          userPoolWebClientId: "x",
+      
+          mandatorySignIn: false,
+      },
+
+      Storage: {
+          AWSS3: {
+            bucket: 'x',
+            region: 'x', 
+          }
         },
 
-        Storage: {
-            AWSS3: {
-              bucket: 'x',
-              region: 'x', 
+      predictions: {
+        convert: {
+          translateText: {
+            region: 'x',
+            proxy: false,
+            defaults: {
+              sourceLanguage: 'en',
+              targetLanguage: 'zh'
             }
           },
-    }
-
+          speechGenerator: {
+            region: 'x',
+            proxy: false,
+            defaults: {
+              VoiceId: 'Ivy',
+              LanguageCode: 'en-US'
+            }
+          },
+          transcription: {
+            region: 'x',
+            proxy: false,
+            defaults: {
+              language: 'en-US'
+            }
+          }
+        },
+        identify: {
+          identifyText: {
+            proxy: false,
+            region: 'x',
+            defaults: {
+              format: 'PLAIN'
+            }
+          },
+          identifyEntities: {
+            proxy: false,
+            region: 'x',
+            celebrityDetectionEnabled: true,
+            defaults: {
+              collectionId: 'identifyEntities8b89c648-test',
+              maxEntities: 50
+            }
+          },
+          identifyLabels: {
+            proxy: false,
+            region: 'x',
+            defaults: {
+              type: 'LABELS'
+            }
+          }
+        },
+        interpret: {
+          interpretText: {
+            region: 'x',
+            proxy: false,
+            defaults: {
+              type: 'ALL'
+            }
+          }
+        }
+      }
+  }
 
 };
